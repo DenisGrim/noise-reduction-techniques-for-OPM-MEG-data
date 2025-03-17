cc4a9d2f200051de18d1c1b661b8dc21d1cc442d
Denis Schmidt <denis.schmidt2003@gmail.com>
2025-03-17 10:33:36 +0100

    removed redundant 'unedited_program'

--- unedited_program/toolbox/func_filtering/Preprocess/pre_zeroreset.m
+++
@@ -1,7 +0,0 @@
-function data = pre_zeroreset(data)
-
-[Nch,Nt] = size(data);
-
-for ch = 1 : Nch
-data(ch,:)=data(ch,:)-mean(data(ch,:),2);
-end
--- unedited_program/toolbox/util/func_plot/set_axis.m
+++
@@ -1,7 +0,0 @@
-function set_axis(h)
-
-if nargin < 1
-    h = gca;
-end
-
-set(h, 'box', 'off', 'tickdir', 'out');
